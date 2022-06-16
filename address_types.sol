9694374f8e66237332f88ec5149a9be079aa7a05
Wellington Adames <wellington.adames.p@gmail.com>
2022-06-16 13:57:10 -0400

    adding address types

---
+++ address_types.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.15;
+
+contract address_types {
+    //Addess types use for manage hash address
+
+    address local;
+    address public local_address = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    address public local_address_2 = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+
+}
